c4312cd035d2bbfb0a2a94528fdb111dde51eb22
Will Farrington <wcfarrington@gmail.com>
2008-06-30 15:03:28 -0400

    Fix up some silly auto* crap

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,9 @@
-echo
-echo "**Error**: autogen.sh has been replaced by bootstrap for the 2.5.x release."
-echo "To bootstrap a 1.9.x build run bootstrap-2.4."
-DIE=1
+#!/bin/sh
+# This is a basic script that runs a glorified gnome-autogen.sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="telepathy-glib-sharp"
+
+. gnome-autogen.sh
